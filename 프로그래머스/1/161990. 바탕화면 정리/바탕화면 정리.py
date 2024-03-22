28ddcab4cf70e03b9e549afb2dc7085d037f3bf5
kswoo619 <83918622+kswoo619@users.noreply.github.com>
2024-03-22 21:42:45 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.16 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,17 +1,8 @@
-def solution(wallpaper):
-    coord = set()
-    answer = []
-    
-    for i, j in enumerate(wallpaper):
-        if '#' in j:
-            coord.add((i, j.find('#')))
-            coord.add((i + 1, j.find('#') + 1))
-            coord.add((i, len(j) - j[::-1].find('#') - 1))
-            coord.add((i + 1, len(j) - j[::-1].find('#')))
-        
-    answer.append(min(coord, key=lambda x: x[0])[0])
-    answer.append(min(coord, key=lambda x: x[1])[1])
-    answer.append(max(coord, key=lambda x: x[0])[0])
-    answer.append(max(coord, key=lambda x: x[1])[1])
-    
-    return answer
+def solution(wall):
+    a, b = [], []
+    for i in range(len(wall)):
+        for j in range(len(wall[i])):
+            if wall[i][j] == "#":
+                a.append(i)
+                b.append(j)
+    return [min(a), min(b), max(a) + 1, max(b) + 1]
