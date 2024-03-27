cad602f2efbc1211459700e7aaad54321332fbf5
kswoo619 <83918622+kswoo619@users.noreply.github.com>
2024-03-27 15:05:35 +0900

    [level 1] Title: 달리기 경주, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,16 +1,10 @@
 def solution(players, callings):
-    rank = dict(zip(players, [i for i in range(len(players))]))
-    inverse = dict(zip([i for i in range(len(players))], players))
-    
-    for i in callings:
-        idx = rank[i]
-        passed = inverse[idx-1]
-        idx2 = rank[passed]
-        
-        rank[i] = idx2
-        rank[passed] = idx
-        
-        rank[passed], rank[i] = idx, idx2
-        inverse[idx], inverse[idx2] = inverse[idx2], inverse[idx]
-    
-    return sorted(rank, key=lambda x:rank[x])
+    pla_dic = {key: i for i, key in enumerate(players)}
+
+    for p in callings:
+        c = pla_dic[p]
+        pla_dic[p] -= 1
+        pla_dic[players[c-1]] += 1
+        players[c-1], players[c] = players[c], players[c-1]
+
+    return players
