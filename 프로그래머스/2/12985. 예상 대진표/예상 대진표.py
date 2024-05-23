e2dcbd00028fd8aec21f2c26fe0703e986be0a2e
kswoo619 <83918622+kswoo619@users.noreply.github.com>
2024-05-23 15:34:25 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,13 @@
+def solution(n,a,b):
+    answer = 0
+    while a != b:
+        if a % 2 != 0:
+            a += 1
+        a /= 2
+        
+        if b % 2 != 0:
+            b += 1
+        b /= 2
+        
+        answer += 1
+    return answer
