aff2598c8b77cdfa69650aeb0b615917f76a8468
kswoo619 <83918622+kswoo619@users.noreply.github.com>
2024-05-30 22:39:32 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,21 @@
+def solution(s):
+    for _ in s:
+        valid = 0
+        stack = []
+        for i in s:
+            if not stack or i in {'(', '[', '{'}:
+                stack.append(i)
+            else:
+                if ( stack[-1] == '[' and i == ']' or
+                    stack[-1] == '{' and i == '}' or
+                    stack[-1] == '(' and i == ')' ):
+                    stack.pop()
+                    if not stack:
+                        valid += 1
+                else:
+                    stack.append(i)
+        print(s, stack, valid)
+        if not stack:
+            break
+        s = s[1:] + s[0]
+    return valid if not stack else 0
