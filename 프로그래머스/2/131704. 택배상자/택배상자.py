606b7dfee993f306209af3b2748b66ee11f88738
kswoo619 <83918622+kswoo619@users.noreply.github.com>
2024-10-04 22:07:01 +0900

    [level 2] Title: 택배상자, Time: 373.54 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -1,16 +1,33 @@
-from collections import deque
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
 def solution(order):
-    stack = deque()
+    answer = 0
+    stacks = []
+    N = len(order)
+    i = 1
     idx = 0
+    while i < N+1:
+        stacks.append(i)
+        while stacks[-1] == order[idx]:
+            idx += 1
+            stacks.pop()
+            if len(stacks) == 0:
+                break
+        i += 1
 
-    for i in range(1, len(order) + 1):
-        stack.append(i)
-        
-        if stack[-1] == order[idx]:
-            while stack and stack[-1] == order[idx]:
-                idx += 1
-                stack.pop()
-        else:
-            continue
-        
+
     return idx
