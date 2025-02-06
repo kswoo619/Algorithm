0188c43b7d119fb843b00853de29cd55b1bdd186
kswoo619 <83918622+kswoo619@users.noreply.github.com>
2025-02-06 20:03:27 +0900

    [level 1] Title: 예산, Time: 2.96 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        if (Arrays.stream(d).sum() <= budget) return d.length;
+        
+        Arrays.sort(d);
+
+        int answer = 0;
+        for(int e:d){
+            budget -= e;
+            if (budget >= 0) answer++;
+        }
+        
+        return answer;
+    }
+}
